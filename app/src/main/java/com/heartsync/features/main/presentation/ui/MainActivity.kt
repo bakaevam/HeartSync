6ba7bc19cd8b1843a9031bf73a7bb2c467f70bad
bakaevam <43135896+bakaevam@users.noreply.github.com>
2023-10-25 09:20:27 +0300

    Merge pull request #1
    * Создание проекта

---
+++ app/src/main/java/com/heartsync/features/main/presentation/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.heartsync.features.main.presentation.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
